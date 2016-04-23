2d83c2ed482d7a4efbb22214289b0af354483c08
Ben Chatelain <ben@octop.ad>
2016-04-23 16:07:45 -0600

    Remove dead code

--- Source/CarthageKit/Xcode.swift
+++ Source/CarthageKit/Xcode.swift
@@ -932,14 +932,6 @@ public func buildScheme(scheme: String, withConfiguration configuration: String,
 							return false
 						}
 					}
-//					.filter { settings in
-//						// Skip building for device when platform requires bitcode but bitcode is disabled
-////						if [.tvOS, .watchOS].contains(sdk.platform) {
-//						if [.tvOS, .watchOS].contains(sdk) && settings.bitcodeEnabled.value == false {
-//							return false
-//						}
-//						return true
-//					}
 					.flatMap(.Concat) { settings -> SignalProducer<TaskEvent<BuildSettings>, CarthageError> in
 						if settings.bitcodeEnabled.value == true {
 							argsForBuilding.bitcodeGenerationMode = .Bitcode
@@ -1026,17 +1018,6 @@ public func buildScheme(scheme: String, withConfiguration configuration: String,
 
 			return SignalProducer(result: filterResult.map { (platform, $0) })
 		}
-//		.flatMap(.Concat) { platform, sdks, settings -> SignalProducer<(Platform, [SDK]), CarthageError> in
-//			// Filter out SDKs that require bitcode
-//
-//			.filter { settings in
-//				// Skip building for device when platform requires bitcode but bitcode is disabled
-//				//						if [.tvOS, .watchOS].contains(sdk.platform) {
-//				if [.tvOS, .watchOS].contains(sdk) && settings.bitcodeEnabled.value == false {
-//					return false
-//				}
-//				return true
-//		}
 		.filter { _, sdks in
 			debugPrint("sdks: \(sdks)")
 			return !sdks.isEmpty
